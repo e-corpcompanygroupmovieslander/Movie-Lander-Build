0adcc96a7e2289d5e19be427903ef408600b5b5d
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-12 10:15:57 +0300

    Updated Home Page

--- app/src/main/assets/library/Connection/Connection.js
+++ app/src/main/assets/library/Connection/Connection.js
@@ -6,7 +6,7 @@ STORE('local','AppVersion',CURRENTVERSION);
 APPMODE(localStorage.getItem('AppColour'),'','#121212');
 BODY();
 ZOOM();
-ENVIRONMENT('');
+ENVIRONMENT('Test');
 
 
 if (localStorage.getItem('Environment') === 'Production') {
