1969a5593fff92c78715db9409c499570fb49305
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-13 07:12:06 +0300

    PAYMENT PAGE LOADING CORRECTED

--- app/src/main/assets/Server/PAGES/PremiumPage/PaymentPage.js
+++ app/src/main/assets/Server/PAGES/PremiumPage/PaymentPage.js
@@ -1,50 +1,44 @@
 import { PREMIUMPAGE } from "./PremiumPage.js";
 
-const PAYMENTPAGE=()=>{
+const PAYMENTPAGE = () => {
 
-    DISPLAY('',`
-
+    DISPLAY('', `
+    
         <header class='AppHeader'>
 
             <img id='BackIcon' class='BackIcon' src='../library/Assets/icon/arrow.png'/>
-
+            
             <h1 class='Titles'>Payments</h1>
 
         </header>
     
         <div id='DataDiv' class='ScrollView'>
 
-            <iframe class='Payment' src='${sessionStorage.getItem('Link')}'/>
-    
-        </div>
+            <img id='HomeLoader' class='LoadingIcon' src='../library/Assets/icon/loading.png'/>
+
+            <iframe id='PaymentFrame' class='Payment' src='${sessionStorage.getItem('Link')}'></iframe>
         
-    `)
+        </div>
+    `);
 
-    DECLARATION('.Payment',(ELEMENT)=>{
-
+    DECLARATION('.Payment', (ELEMENT) => {
         // Access data from local storage
         var localStorageData = localStorage.getItem('UserData');
 
         // Pass data to the iframe
-        
         ELEMENT.onload = function() {
             ELEMENT.contentWindow.postMessage(localStorageData, '*');
+            // Once loaded, remove the loading message
+            document.getElementById('HomeLoader').style.display = 'none';
         };
-           
-    })
+    });
 
-    DECLARATION('.BackIcon',(ELEMENT)=>{
-    
-        EVENT(ELEMENT,'click',()=>{
+    DECLARATION('.BackIcon', (ELEMENT) => {
+        EVENT(ELEMENT, 'click', () => {
+            REMOVESTORE('', 'Link');
+            PREMIUMPAGE();
+        });
+    });
+};
 
-        REMOVESTORE('','Link')
-
-        PREMIUMPAGE()
-
-        })
-
-    })
-    
-}
-
-export{PAYMENTPAGE}
+export { PAYMENTPAGE };
