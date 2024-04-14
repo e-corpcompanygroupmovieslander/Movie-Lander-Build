8a4614f6d9a09e1f020c3db42b70121e8c0704cf
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-14 05:32:15 +0300

    AUTO PREMIUM USER PAGE

--- app/src/main/assets/Server/PAGES/PremiumAccountChecker/PremiumAccountChecker.js
+++ app/src/main/assets/Server/PAGES/PremiumAccountChecker/PremiumAccountChecker.js
@@ -1,16 +1,18 @@
 const AUTOPREMIUMUSER = () => {
     GETPACKAGE(MTNPREMIUMPAYGET, 'cors', (data) => {
-        FINDER(data, 'User', localStorage.getItem('User'), (user) => {
-            if (new Date() <= new Date(user.ExpiryDate)) {
-                CHECK(user, (result) => {
-                    STORE('local', 'PremiumUser', JSON.stringify(result));
-                    STORE('local', 'Premium', 'ON');
-                });
-            } else {
-                REMOVESTORE('local', 'PremiumUser');
-                REMOVESTORE('local', 'Premium');
-            }
-        });
+        REVERSE(data,(result)=>{
+            FINDER(result, 'User', localStorage.getItem('User'), (user) => {
+                if (new Date() <= new Date(user.ExpiryDate)) {
+                    CHECK(user, (result) => {
+                        STORE('local', 'PremiumUser', JSON.stringify(result));
+                        STORE('local', 'Premium', 'ON');
+                    });
+                } else {
+                    REMOVESTORE('local', 'PremiumUser');
+                    REMOVESTORE('local', 'Premium');
+                }
+            });
+        });    
     });
 }
 
