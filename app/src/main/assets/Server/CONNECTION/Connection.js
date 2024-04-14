640482e9b461bcf3f27fd44b17323304cd0939be
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-14 05:47:23 +0300

    ACCOUNT SECTION WORKED ON

--- app/src/main/assets/Server/CONNECTION/Connection.js
+++ app/src/main/assets/Server/CONNECTION/Connection.js
@@ -1,12 +1,12 @@
 import { BUSINESSTOOL } from "../FUNCTIONS/BusinessTools/BusinessTools.js"
 import { COMMUNITY } from "../FUNCTIONS/Community/Community.js"
 import { NOTIFICATIONS } from "../FUNCTIONS/Notifications/Notifications.js"
+import { USERSETTINGSACCOUNTPAGEPREMIUM } from "../PAGES/AccountsSettingsPage/PremiumAccountPage.js"
 import { AUTODELETEACCOUNT } from "../PAGES/AutoDeleteAccount/AutoDeleteAccount.js"
 import { AUTODEVICE } from "../PAGES/AutoDevice/AutoDevice.js"
 import { HOMEPAGE } from "../PAGES/HomePage/HomePage.js"
 import { MOVIESDETAILSPAGE } from "../PAGES/MoviesDetailsPage/MoviesDetailsPage.js"
 import { AUTOPREMIUMUSER } from "../PAGES/PremiumAccountChecker/PremiumAccountChecker.js"
-import { USERSETTINGSACCOUNTPAGEPREMIUM } from "../PAGES/PremiumAccountPage/PremiumAccountPage.js"
 import { PREMIUMPAGE } from "../PAGES/PremiumPage/PremiumPage.js"
 import { LIBRARY } from "../PAGES/libraryPage/LibraryPage.js"
 
---
+++ app/src/main/assets/Server/PAGES/AccountsSettingsPage/PremiumAccountPage.js
@@ -0,0 +1,50 @@
+const USERSETTINGSACCOUNTPAGEPREMIUM=()=>{
+
+    DECLARATION('#DataDiv',(ELEMENT)=>{
+
+        DISPLAY(ELEMENT,`
+
+            <button id='AppLock' class='Button'>
+
+                <h1 class='Buttontitle'>My Premium Account</h1>
+
+                <img class='ButtonIcon' src='../library/Assets/icon/subscription.png'/>
+            
+            </button>
+
+            <button id='AppLock' class='Button'>
+
+                <h1 class='Buttontitle'>Movie Lander Updates</h1>
+
+                <img class='ButtonIcon' src='../library/Assets/icon/trending-topic.png'/>
+            
+            </button>
+
+            <button id='AppLock' class='Button'>
+
+                <h1 class='Buttontitle'>Devices</h1>
+
+                <img class='ButtonIcon' src='../library/Assets/icon/devices.png'/>
+            
+            </button>
+
+            <button id='AppLock' class='Button'>
+
+                <h1 class='Buttontitle'>My Account</h1>
+
+                <img class='ButtonIcon' src='../library/Assets/icon/bottom.png'/>
+            
+            </button>
+
+        
+        
+        
+        
+        `)
+
+    })
+
+
+}
+
+export{USERSETTINGSACCOUNTPAGEPREMIUM}
