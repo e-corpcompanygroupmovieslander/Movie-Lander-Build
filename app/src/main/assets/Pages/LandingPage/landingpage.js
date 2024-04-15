caee5e7447ae15e43976c645513a8d2242d3ed16
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-15 16:20:22 +0300

    Landing Page Created

---
+++ app/src/main/assets/Pages/LandingPage/landingpage.js
@@ -0,0 +1,63 @@
+DISPLAY('',`
+
+<header class='AppHeader'>
+
+    <img id='AppimageSection' class='BackIcon' src='../../library/Assets/images/playstore.png'/>
+
+    <h1 id='Section' class='Titles'>Download</h1>
+
+</header>
+
+<div id='PolicyDataDiv' class='ScrollView'>
+
+    <img  class='AppLogo' src='../../library/Assets/images/playstore.png'/>
+
+    <div class='ViewHolder'>
+
+        <div id='ImageHolder' class='View'>
+        
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.03.21.jpeg'/>
+
+        </div>
+
+        <div id='ImageHolder' class='View'>
+        
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.03.47 (1).jpeg'/>
+
+        </div>
+
+        <div id='ImageHolder' class='View'>
+            
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.03.47.jpeg'/>
+
+        </div>
+
+        <div id='ImageHolder' class='View'>
+        
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.04.22.jpeg'/>
+
+        </div>
+
+        <div id='ImageHolder' class='View'>
+            
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.03.21.jpeg'/>
+
+        </div>
+
+        <div id='ImageHolder' class='View'>
+        
+            <img  class='inAppImages' src='../../library/Assets/images/landingpage/WhatsApp Image 2024-04-15 at 16.03.47.jpeg'/>
+
+        </div>
+
+    </div>
+
+    <h2>Get The Latest Cinema Experience </h2>
+
+    <button>Download</button>
+
+    
+
+</div>
+
+`)
