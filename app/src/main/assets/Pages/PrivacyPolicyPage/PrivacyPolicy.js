f6650f2a779b28415bd6dd3ef04a9346b07e59a9
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-15 13:51:50 +0300

    APP PRIVACY POLICY PAGE INTRODUCED

---
+++ app/src/main/assets/Pages/PrivacyPolicyPage/PrivacyPolicy.js
@@ -0,0 +1,17 @@
+DISPLAY('',`
+
+<header>
+
+    <img id='AppimageSection' class='BackIcon' src='../../library/Assets/images/playstore.png'/>
+
+    <h1 id='Section' class='Titles'>Movie Lander Policies</h1>
+
+</header>
+
+<div id='PolicyDataDiv' class='ScrollView'>
+
+    <img id='HomeLoader' class='LoadingIcon' src='../../library/Assets/icon/loading.png'/>
+
+</div>
+
+`)
