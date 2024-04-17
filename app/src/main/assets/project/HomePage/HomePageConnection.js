20b44143f7b0ddeec83e1a512c6180e41f60674d
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-17 12:53:53 +0300

    MORE SECTIONS HOME PAGE CLICKS

--- app/src/main/assets/project/HomePage/HomePageConnection.js
+++ app/src/main/assets/project/HomePage/HomePageConnection.js
@@ -11,7 +11,119 @@ HOMEPAGECONNECTION=()=>{
     EXTERNALJS(HOMEHORRORAPI,()=>{HOMEHORROR()});
     EXTERNALJS(LATESTROMANCEAPI,()=>{LATESTROMANCE()})
 
-    
-    
+    //MORE SECTIONS CLICKED
+    CLICKED('#MoreAnimations',()=>{
+
+        const DATA={
+            "Sections":"Animations",
+            "link":ANIMATIONAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
+
+    CLICKED('#MoreMovies',()=>{
+
+        const DATA={
+            "Sections":"Action",
+            "link":MOVIESAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
+
+    CLICKED('#MoreMarathons',()=>{
+
+        const DATA={
+            "Sections":"Marathons",
+            "link":MARATHONAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
+
+    CLICKED('#MoreSeries',()=>{
+
+        const DATA={
+            "Sections":"Series",
+            "link":SERIESAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
+
+    CLICKED('#MoreAdventure',()=>{
+
+        const DATA={
+            "Sections":"Adventure",
+            "link":ADVENTUREAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
+
+    CLICKED('#MoreHorror',()=>{
+
+        const DATA={
+            "Sections":"Horror",
+            "link":HORRORAPI,
+        }
+
+        JSONIFICATION(DATA,(data)=>{
+
+            STORE('','Path','SeeMore')
+
+            STORE('','MoviesData',data);
+
+            EXTERNALJS('../project/CatergoriesSelectionPage/CatergorySelectionPage.js',()=>{CATERGORIESSELECTIONPAGE()});
+
+        })
+
+    })
 
 }
