583ad5cd288801ac4c89796c1e6c0a830f14c026
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-17 08:12:46 +0300

    HOMEANIMATION LOCALIZED

--- app/src/main/assets/app/HomePageApi.js
+++ app/src/main/assets/app/HomePageApi.js
@@ -1,3 +1,4 @@
 const HOMEMOVIESPAGEAPI='../project/HomePage/HomePage.txt';
 const HOMEPAGECONNECTIONAPI='../project/HomePage/HomePageConnection.js';
-const LATESTANIMATIONAPI='../project/HomePage/HomePageMovies/latestAmination/latestAnimation.js'
+const LATESTANIMATIONAPI='../project/HomePage/HomePageMovies/latestAmination/latestAnimation.js';
+const HOMEANIMATIONAPI='../project/HomePage/HomePageMovies/HomeAnimation/HomeAnimations.js';
