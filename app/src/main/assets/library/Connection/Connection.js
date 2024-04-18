3e6d033896da4ab4beabef50e4cacc82fb9b36fe
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-18 14:59:00 +0300

    APP VERSION CORRECTED

--- app/src/main/assets/library/Connection/Connection.js
+++ app/src/main/assets/library/Connection/Connection.js
@@ -37,7 +37,7 @@ if (localStorage.getItem('Environment') === 'Production') {
 
 const APPSTART = () => {
 
-    CONDITION( VERSIONSTATUS === 'Minor' && APPVERSION > CURRENTVERSION || APPVERSION === CURRENTVERSION  ,
+    CONDITION( VERSIONSTATUS === 'Minor',
 
     () => CONDITION(localStorage.getItem('AppLock'),
 
